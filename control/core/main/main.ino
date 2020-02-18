86026b89b8d3b8c821c4c214fe46871e1ab3ccc5
cvillarr23 <cdvillarreal@wisc.edu>
2020-02-17 18:42:22 -0600

    control system added

---
+++ control/core/main/main.ino
@@ -0,0 +1,25 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+/**
+ * Pumps a specified amount of water into the soil
+ */
+void pumpWater(int sec) {
+  
+}
+
+/**
+ * Checks water level
+ * 
+ */
+int checkLevel() {
+  
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
